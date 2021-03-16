d691ee20521a8db99a325174eaf39b05873e756c
Jonathan Terrell <jonathan.terrell@stewartorg.com>
2021-03-16 09:48:21 -0500

    While function to confirm individual activities working correctly. While loop to confirm Day trip working correctly

--- daytripgen.js
+++ daytripgen.js
@@ -6,12 +6,12 @@ function randomString(theArray){
 function confirmRandom(string, array){
     let choose = window.confirm("Do you want to go to " + string + "?")
 while(choose === false){
-    let string = randomString(array);
+    string = randomString(array);
     choose = window.confirm("Do you want to go to " + string + "?");
-}
- console.log(string);
-}
 
+}return string;
+
+}
 
 
 let destination = ["Dallas",  "Houston", "San Antonio", "South Padre Island", "Amarillo"];
@@ -19,20 +19,35 @@ let restaurant = ["Texas Roadhouse", "Gloria's", "Olive Garden", "Snuffer's", "P
 let transportation = ["Truck", "Plane", "Helicopter", "Bus", "Horse"];
 let entertainment = ["Movie", "Bowling", "Bar", "Baseball Game", "Dancing"];
 
+
+//let yourDestination = randomString(destination);
+//let choose = window.confirm("Do you want to go to " + yourDestination + "?");
+
+//while(choose === false){
+   // let yourDestination = randomString(destination);
+    //choose = window.confirm("Do you want to go to " + yourDestination + "?");
+//}
+ //console.log(yourDestination);
+
 let yourDestination = randomString(destination);
-let choose = window.confirm("Do you want to go to " + yourDestination + "?");
-
-while(choose === false){
-    let yourDestination = randomString(destination);
-    choose = window.confirm("Do you want to go to " + yourDestination + "?");
-}
- console.log(yourDestination);
-
+let finalDestination = confirmRandom(yourDestination, destination);
 let yourRestaurant = randomString(restaurant);
-confirmRandom(yourRestaurant, restaurant);
+let finalRestaurant = confirmRandom(yourRestaurant, restaurant);
 let yourTransportation = randomString(transportation);
-console.log(yourTransportation);
+let finalTransportation = confirmRandom(yourTransportation, transportation);
 let yourEntertainment = randomString(entertainment);
-console.log(yourEntertainment);
+let finalEntertainment = confirmRandom(yourEntertainment, entertainment);
+let confirmDayTrip = window.confirm("Please confirm your Day Trip to " + finalDestination + ". You will be arriving by " + finalTransportation + ". You will be eating dinner at " + finalRestaurant + ". The entertainment for the evening will be " + finalEntertainment + ".");
 
-
+while(confirmDayTrip === false){
+    yourDestination = randomString(destination);
+    finalDestination = confirmRandom(yourDestination, destination);
+    yourRestaurant = randomString(restaurant);
+    finalRestaurant = confirmRandom(yourRestaurant, restaurant);
+    yourTransportation = randomString(transportation);
+    finalTransportation = confirmRandom(yourTransportation, transportation);
+    yourEntertainment = randomString(entertainment);
+    finalEntertainment = confirmRandom(yourEntertainment, entertainment);
+    confirmDayTrip = window.confirm("Please confirm your Day Trip to " + finalDestination + ". You will be arriving by " + finalTransportation + ". You will be eating dinner at " + finalRestaurant + ". The entertainment for the evening will be " + finalEntertainment + ".");
+}
+console.log("Your destination is " + finalDestination + ". You will arrive by " + finalTransportation + ". You will be eating dinner at " + finalRestaurant + ". The evenings entertainment will be " + finalEntertainment + ".");
