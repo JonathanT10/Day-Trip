c691193849f1d29af64d249d99923c27e85c0e24
Jonathan Terrell <jonathan.terrell@stewartorg.com>
2021-03-15 21:52:33 -0500

    Working on confirmRandom function. Currently will console.log the firs choice even if cancled.

--- daytripgen.js
+++ daytripgen.js
@@ -3,7 +3,14 @@ function randomString(theArray){
     result = theArray[Math.floor(Math.random()*theArray.length)];
     return result;
 }
-
+function confirmRandom(string, array){
+    let choose = window.confirm("Do you want to go to " + string + "?")
+while(choose === false){
+    let string = randomString(array);
+    choose = window.confirm("Do you want to go to " + string + "?");
+}
+ console.log(string);
+}
 
 
 
@@ -13,8 +20,7 @@ let transportation = ["Truck", "Plane", "Helicopter", "Bus", "Horse"];
 let entertainment = ["Movie", "Bowling", "Bar", "Baseball Game", "Dancing"];
 
 let yourDestination = randomString(destination);
-let choose = window.confirm("Do you want to go to " + yourDestination + "?")
-console.log(choose)
+let choose = window.confirm("Do you want to go to " + yourDestination + "?");
 
 while(choose === false){
     let yourDestination = randomString(destination);
@@ -23,7 +29,7 @@ while(choose === false){
  console.log(yourDestination);
 
 let yourRestaurant = randomString(restaurant);
-console.log(yourRestaurant);
+confirmRandom(yourRestaurant, restaurant);
 let yourTransportation = randomString(transportation);
 console.log(yourTransportation);
 let yourEntertainment = randomString(entertainment);
