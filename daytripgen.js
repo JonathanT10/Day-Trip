704ccced9cf54dc6f5967b133e5ef83ad8225cd1
Jonathan Terrell <jonathan.terrell@stewartorg.com>
2021-03-15 20:14:15 -0500

    Add array's for destination, restaurant, transportation, and entertainment

--- daytripgen.js
+++ daytripgen.js
@@ -8,7 +8,18 @@ function randomString(theArray){
 
 
 let destination = ["Dallas",  "Houston", "San Antonio", "South Padre Island", "Amarillo"];
+let restaurant = ["Texas Roadhouse", "Gloria's", "Olive Garden", "Snuffer's", "Primo's", "Pecan Lodge"];
+let transportation = ["Truck", "Plane", "Helicopter", "Bus", "Horse"];
+let entertainment = ["Movie", "Bowling", "Bar", "Baseball Game", "Dancing"];
 
 let yourDestination = randomString(destination);
 console.log(yourDestination);
+let yourRestaurant = randomString(restaurant);
+console.log(yourRestaurant);
+let yourTransportation = randomString(transportation);
+console.log(yourTransportation);
+let yourEntertainment = randomString(entertainment);
+console.log(yourEntertainment);
+
+
 
